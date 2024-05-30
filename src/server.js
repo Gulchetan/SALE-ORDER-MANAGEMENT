ac27177c74440ae13b14f004a124bd80eb396a0c
Gulchetan <gulchetanisgood@gmail.com>
2024-05-30 13:52:55 +0530

    added

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/api/data', (req, res) => {
+  const data = fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8');
+  res.json(JSON.parse(data));
+});
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
